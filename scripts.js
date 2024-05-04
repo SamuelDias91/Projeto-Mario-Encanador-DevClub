66a99cc38cf197c2224da9ffe629dccbed86adeb
SamuelDias91 <samueldiasdev@gmail.com>
2024-05-04 18:03:26 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+function mostrarForm () {
+form.style.left = "40%"
+form.style.transform = "translateX(-50px)"
+mascara.style.visibility = "visible"
+}
+
+function ocultarForm () {
+form.style.left = "-300px"
+form.style.transform = "translateX(0)"
+mascara.style.visibility ="hidden"
+}
